8a2a2d7d56b7a02faba64405f79a052d368fc49f
Desmond Weindorf <desmondweindorf@gmail.com>
2022-07-25 12:46:05 -0700

    ez

---
+++ 852.peak-index-in-a-mountain-array.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=852 lang=python3
+#
+# [852] Peak Index in a Mountain Array
+#
+
+# @lc code=start
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        return arr.index(max(arr))
+# @lc code=end
+
