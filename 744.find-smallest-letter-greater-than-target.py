dfa13e4fe298264cef968df4b8d6fd6a754ff615
Desmond Weindorf <desmondw@users.noreply.github.com>
2022-08-08 17:05:45 -0700

    rush

---
+++ 744.find-smallest-letter-greater-than-target.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=744 lang=python3
+#
+# [744] Find Smallest Letter Greater Than Target
+#
+
+# @lc code=start
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        for l in letters:
+            if l > target: return l
+        return letters[0]
+
+# @lc code=end
+
