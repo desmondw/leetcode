98364fb668d4a6a46392ec045af60eaaa39567f3
Desmond Weindorf <desmondw@users.noreply.github.com>
2022-08-18 17:37:01 -0700

    ugh

---
+++ 13.roman-to-integer.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode id=13 lang=python3
+#
+# [13] Roman to Integer
+#
+
+# @lc code=start
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+        subValues = {
+            'IV': 4,
+            'IX': 9,
+            'XL': 40,
+            'XC': 90,
+            'CD': 400,
+            'CM': 900,
+        }
+        total = 0
+
+        for k,v in subValues.items():
+            while k in s:
+                s = s.replace(k,'')
+                total += v
+        for c in s:
+            total += values[c]
+        return total
+
+# @lc code=end
+
