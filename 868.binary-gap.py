db424277b13cff22604b4e5bbd021341fbc2ab08
Desmond Weindorf <desmondw@users.noreply.github.com>
2022-10-18 10:15:30 -0700

    didn't read the spec well enough first...

---
+++ 868.binary-gap.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=868 lang=python3
+#
+# [868] Binary Gap
+#
+
+# @lc code=start
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        # s = bin(n)[2:].strip('0').strip('1')
+        # s = s.split('1')
+        # return max(s, key=lambda v: len(v))
+
+# @lc code=end
+
