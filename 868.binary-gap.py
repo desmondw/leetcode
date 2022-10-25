64f64b4be233832d9f4374d0f196f1bfee8ec7ae
Desmond Weindorf <desmondw@users.noreply.github.com>
2022-10-25 12:29:28 -0700

    in time

--- 868.binary-gap.py
+++ 868.binary-gap.py
@@ -7,9 +7,20 @@
 # @lc code=start
 class Solution:
     def binaryGap(self, n: int) -> int:
-        # s = bin(n)[2:].strip('0').strip('1')
-        # s = s.split('1')
-        # return max(s, key=lambda v: len(v))
+        longest = 0
+        s = bin(n)[2:].strip('0')
+        if len(s) < 2:
+            return 0
+
+        cur = 1
+        for c in s:
+            if c == '0':
+                cur += 1
+            else:
+                if cur > longest:
+                    longest = cur
+                cur = 1
+        return longest
 
 # @lc code=end
 
