6eb3fbd1eae14d346222ff40ca93292a4564b713
Desmond Weindorf <desmondw@users.noreply.github.com>
2023-01-26 13:55:57 -0800

    ezpz

---
+++ 1389.create-target-array-in-the-given-order.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=1389 lang=python3
+#
+# [1389] Create Target Array in the Given Order
+#
+
+# @lc code=start
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        target = []
+        for i in range(len(nums)):
+            target.insert(index[i], nums[i])
+        return target
+# @lc code=end
+
